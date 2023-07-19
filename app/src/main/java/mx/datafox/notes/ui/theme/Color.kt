568736da5a466e46ff538732cfffc21f508adf76
Datafox-JCP <datafox@outlook.com>
2023-07-19 14:16:10 -0600

    Initial commit - Arquitecture & Theme - Dependencies

---
+++ app/src/main/java/mx/datafox/notes/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package mx.datafox.notes.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xFF202020)
+val LightGray = Color(0xFFE4DFDF)
+val DarkBlue = Color(0xFF364C57)
+val LightBlue = Color(0xFFD7E8DE)
+
+val RedOrange = Color(0x9FFFA000)
+val RedPink = Color(0xFFBD7B91)
+val BabyBlue = Color(0xFF81DEEA)
+val Violet = Color(0xFFFFCF94DA)
+val LightGreen = Color(0xFFCBD181)
