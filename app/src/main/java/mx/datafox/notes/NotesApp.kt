6f1f3883cbbbeb4cb483736ffba6de0f06bcba36
Datafox-JCP <datafox@outlook.com>
2023-07-19 15:53:07 -0600

    - Dagger implementation

---
+++ app/src/main/java/mx/datafox/notes/NotesApp.kt
@@ -0,0 +1,8 @@
+package mx.datafox.notes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NotesApp: Application() {
+}
--- app/src/main/java/mx/datafox/notes/notes/data/source/NoteDatabase.kt
+++ app/src/main/java/mx/datafox/notes/notes/data/source/NoteDatabase.kt
@@ -1,13 +1,18 @@
 package mx.datafox.notes.notes.data.source
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import mx.datafox.notes.notes.domain.model.Note
 
 @Database(
     entities = [Note::class],
     version = 1
 )
-abstract class NoteDatabase {
+abstract class NoteDatabase: RoomDatabase() {
 
     abstract val noteDao: NoteDao
+
+    companion object {
+        const val DATABASE_NAME = "notes_db"
+    }
 }
